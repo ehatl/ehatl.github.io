ce9c83d408bbf539cb3deaf606ff2a54e25513a5
Ulises Ehatl Ju?rez Figueroa <ehatlujf@gmail.com>
2021-08-09 13:43:52 -0500

    Initial commit

--- postcards.R
+++ postcards.R
@@ -1,2 +1,6 @@
 # Con use_git() preguntará si desean hacer un commit, y después pedirá reiniciar Rstudio para que obtengan un nuevo botón llamado "git()"
 usethis::use_git()
+
+usethis::use_github()
+
+postcards::create_postcard(template = "onofre")
